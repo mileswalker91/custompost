8fa2dae16458d1df5c638973f9eac9aa096db4d7
Miles Walker <miles.walker@firstfound.co.uk>
2018-03-06 17:15:32 +0000

    Initial Commit

---
+++ wp-content/themes/custom/header.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Blog Template for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+
+
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <?php wp_head();?>
+  </head>
+
+  <body>
+
+
+        <nav class="blog-nav">
+          <a class="blog-nav-item active" href="#">Home</a>
+          <a class="blog-nav-item" href="#">New features</a>
+          <a class="blog-nav-item" href="#">Press</a>
+          <a class="blog-nav-item" href="#">New hires</a>
+          <a class="blog-nav-item" href="#">About</a>
+        </nav>
+
+<div class="row header">
+  <div class="container">
+
+<div class="title"><h1>main title here big heading</h1></div>
+<div class="sidepanel"><p>A smallish sentence here hehe haha</P><img src="http://placehold.it/350x150"></div>
+
+  </div>
+</div>
---
+++ wp-content/themes/custom/index.php
@@ -0,0 +1,33 @@
+<?php get_header(); ?>
+
+	<div class="row">
+
+		<div class="col-sm-8 blog-main">
+
+ <div class="header">
+   <h1>first attmept at ACF</h1>
+   <h2>please work xo </h2>
+   <img src="http://placehold.it/350x150" alt="" />
+ </div>
+
+ <p>some long description here xo</p>
+
+<ul>
+  <li> <img src="http://placehold.it/350x150" alt="" /></li>
+  <li> <img src="http://placehold.it/350x150" alt="" /></li>
+  <li> <img src="http://placehold.it/350x150" alt="" /></li>
+  <li> <img src="http://placehold.it/350x150" alt="" /></li>
+  <li> <img src="http://placehold.it/350x150" alt="" /></li>
+</ul>
+
+
+
+
+
+		</div> <!-- /.blog-main -->
+
+
+
+	</div> <!-- /.row -->
+
+<?php get_footer(); ?>
